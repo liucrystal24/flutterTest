457ac2edf54d534a078bedb754411956359e7a97
liucrystal24 <liucrystal_24@outlook.com>
2019-07-29 17:03:35 +0800

    master add

--- demo1/lib/main.dart
+++ demo1/lib/main.dart
@@ -10,7 +10,7 @@ class TestHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('dev branch test')),
+      appBar: AppBar(title: Text('master add')),
       body: ListView(
         children: <Widget>[
           ListTile(
