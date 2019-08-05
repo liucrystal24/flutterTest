19b08162cb2a9a2b4da69783ad4250cdafcc2b32
liucrystal24 <liucrystal_24@outlook.com>
2019-08-05 17:08:27 +0800

    ExpansionPanelList add

--- demo1/lib/CexpansionPanelList.dart
+++ demo1/lib/CexpansionPanelList.dart
@@ -6,14 +6,60 @@ class ExpansionPanelListdemo extends StatefulWidget {
 }
 
 class _ExpansionPanelListdemoState extends State<ExpansionPanelListdemo> {
+  List<int> mlist;
+  List<ExpansionBean> expansionlist;
+
+  //这里直接调用这个类？？叫什么 与initstate区别？
+  _ExpansionPanelListdemoState(){
+    mlist = new List();
+    expansionlist = new List();
+    for(int i= 0;i<10;i++){
+      mlist.add(i);
+      //类型与类申明一致
+      expansionlist.add(ExpansionBean(i, false));
+    }
+  }
+
+  _currentState(index,bol){
+    setState(() {
+      expansionlist.forEach((item){
+        if (item.index == index){
+          item.isOpen = !bol;
+        }
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('ExpansionPanelList')),
-      body: Center(
-        child: ,
-      ),
+      body: SingleChildScrollView(
+        //ExpansionList 一定要放在可以滚动的widget里
+        child: ExpansionPanelList(
+          expansionCallback: (index,bol){
+             _currentState(index,bol);
+          },
+          //根据list的个数，插入 ExpansionPanel,并继续生成List
+          children: mlist.map((index){
+            return ExpansionPanel(
+              //什么时候是(){},什么时候是{}/()
+              headerBuilder: (context,bol){
+                return ListTile(title: Text('This is No.$index'));
+              },
+              body: ListTile(title: Text('$index detail')),
+              isExpanded: expansionlist[index].isOpen
+            );
+          }).toList()
+        ),
+      )
     );
   }
 }
 
+//??为什么要申明这个类，每个小项的开关管理
+class ExpansionBean{
+  var index;
+  bool isOpen;
+  ExpansionBean(this.index,this.isOpen);
+}
